c147ab7f1368c6e36614b30140d0b6b62c379803
santiago <santiago59782@gmail.com>
2026-06-18 14:02:34 -0500

    firts commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,14 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { MenuNavegacion } from './componentes/menu-navegacion/menu-navegacion';
+import { Contacto } from './contacto/contacto';
+import { Inicio } from './inicio/inicio';
+import { Proyectos } from './proyectos/proyectos';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [MenuNavegacion, Inicio, Proyectos, Contacto],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('portafolio');
---
+++ src/app/componentes/menu-navegacion/menu-navegacion.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-navegacion',
+  imports: [],
+  templateUrl: './menu-navegacion.html',
+  styleUrl: './menu-navegacion.css',
+})
+export class MenuNavegacion {}
